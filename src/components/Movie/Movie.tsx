96e7ef2b7aa581c3a7f2a83ffb720a0936f248f4
Moriarty <116695498+Moriartymath@users.noreply.github.com>
2024-07-09 23:10:17 +0300

    Implement Movie component - Add styling to Movie component

--- src/components/Movie/Movie.tsx
+++ src/components/Movie/Movie.tsx
@@ -1,5 +1,18 @@
-function Movie() {
-  return <li></li>;
+import styles from "./Movie.module.css";
+
+function Movie({ poster, title, stats, deleteButton }) {
+  return (
+    <li className={styles.movie}>
+      <div className={styles.imageContainer}>
+        <img src={poster} alt={title} className={styles.image} />
+      </div>
+      <div className={styles.movieShortDescr}>
+        <h5>{title}</h5>
+        <p>{stats}</p>
+      </div>
+      {deleteButton ? deleteButton : null}
+    </li>
+  );
 }
 
 export default Movie;
